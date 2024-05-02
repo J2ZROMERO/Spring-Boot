e652891f4c947d526e2d7bf903522f6ed626d3d2
j2zromero <jose-zromero@hotmail.com>
2024-05-01 23:03:49 -0600

    Set up mongo & dependencies

---
+++ src/main/java/com/j2zromero/moviesapi/MoviesApiApplication.java
@@ -0,0 +1,15 @@
+package com.j2zromero.moviesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class MoviesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j2zromero/moviesapi/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j2zromero.moviesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
